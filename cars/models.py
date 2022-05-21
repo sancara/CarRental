d5f18ac2718d3bb360747fe943c9de5f8b97c605
Sancara <santiagocaraballo@gmail.com>
2022-05-20 23:57:31 -0300

    create validators for stars in review

--- cars/models.py
+++ cars/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Review(models.Model):
     first_name = models.CharField(max_length=30)
     email = models.EmailField()
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     
