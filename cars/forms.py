253946dfb864ac7ef3247e00044a0835fac63ac7
Sancara <santiagocaraballo@gmail.com>
2022-05-20 23:28:36 -0300

    create ModelForm

--- cars/forms.py
+++ cars/forms.py
@@ -1,7 +1,13 @@
 from django import forms 
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
+# class ReviewForm(forms.Form):
 
-    first_name = forms.CharField(label='First Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    review = forms.CharField(label='Please write the review', widget=forms.Textarea())
+#     first_name = forms.CharField(label='First Name', max_length=100)
+#     email = forms.EmailField(label='Email')
+#     review = forms.CharField(label='Please write the review', widget=forms.Textarea())
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
