8de401368e50fe1742d0b173d0775faeefbdda54
Sancara <santiagocaraballo@gmail.com>
2022-05-20 23:37:37 -0300

    ModelForm: fields

--- cars/models.py
+++ cars/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Review(models.Model):
     first_name = models.CharField(max_length=30)
-    email = models.EmailField
+    email = models.EmailField()
     
